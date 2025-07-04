a6a0023e7ef17c3c4cf8648e714f458866ee4113
Yoon Sejin <pentum99@gmail.com>
2025-07-04 16:45:58 +0900

    fix

---
+++ js/myjs.js
@@ -0,0 +1,12 @@
+function updateF1() {
+    document.getElementById("f1").innerHTML = "change1";
+}
+function refreshF1() {
+    document.getElementById("f1").innerHTML = "변경될 내용1";
+}
+function updateF2() {
+    document.getElementById("f2").innerHTML = "change2";
+}
+function refreshF2() {
+    document.getElementById("f2").innerHTML = "변경될 내용2";
+}
